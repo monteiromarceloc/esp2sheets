79f5ee091eb21ca827f7a862518059c56c891c97
Marcelo Monteiro <monteiromarceloc@gmail.com>
2020-03-08 20:59:45 -0300

    esp base code

---
+++ espcode/espcode.ino
@@ -0,0 +1,28 @@
+
+#define BOMBA 1
+#define LEDS 2
+#define SEN_LUZ 3
+#define SEN_TEMP 4
+#define SEN_TEMP_AR 5
+#define SEN_HUM_AR 6
+#define SEN_HUM_SOLO 7
+#define EXAUSTOR 8
+
+void setup() {
+  // wifi connectoin
+}
+
+void loop() {
+  // get sensors data
+  // get global time
+
+  // set led
+  // set bomb
+  // set exaustor
+
+  // timer bomba
+  // timer exaustor
+
+  // send data via wifi
+
+}
